22251e41637acb8158b70c901191bb90cbaf42fc
std6122420214 <std.67122420214@ubru.ac.th>
2026-03-11 22:11:50 +0700

    เเก้

--- formulaone/core/routes.py
+++ formulaone/core/routes.py
@@ -1,19 +1,22 @@
 from flask import Blueprint, render_template, request
 from formulaone.extensions import db
-from formulaone.models import FormulaOne, Team
-core_bp = Blueprint('core', __name__, template_folder='templates')
+from formulaone.models import FormulaOne
 
-@core_bp.route('/')
+core_bp = Blueprint("core", __name__)
+
+
+@core_bp.route("/")
 def index():
-  page = request.args.get('page',type=int)
-  formulaones = db.paginate(db.select(formulaone), per_page=4, page=page)
-  return render_template('core/index.html',
-                         title='Home Page',
-                         formulaones=formulaones)
 
-@core_bp.route('/<int:id>/detail')
-def detail(id):
-  formulaone = db.session.get(formulaone, id)
-  return render_template('core/formulaone_detail.html',
-                         title='formulaone Detail Page',
-                         formulaone=formulaone)
+    page = request.args.get("page", 1, type=int)
+
+    formulaones = db.paginate(
+        db.select(FormulaOne),
+        per_page=4,
+        page=page
+    )
+
+    return render_template(
+        "index.html",
+        formulaones=formulaones
+    )
